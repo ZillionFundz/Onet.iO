2e43797f2b7db4311c28b42bbe4e5d64e939000f
ZillionFundz <123439025+ZillionFundz@users.noreply.github.com>
2025-08-22 02:39:55 -0700

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,3 +7,28 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+
+
+// TO INITIALISE SWIPER:
+const swiper = new Swiper('.wrapper', {
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    // },
+});
